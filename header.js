b2b2c34caf237c0463b3350627c742094928fd01
Benjamin Hunsicker <benhunsicker1@gmail.com>
2023-11-07 18:22:07 -0500

    Update all portfolio subpages
    Mobile-ready and responsive itchio iframes

--- header.js
+++ header.js
@@ -41,6 +41,33 @@ class HeaderPort extends HTMLElement {
 	}
 }
 
+class HeaderAbout extends HTMLElement {
+	constructor() {
+	  super();
+	}
+  
+	connectedCallback() {
+	  this.innerHTML = `
+	  <center>
+	  <p><img src="/benthehun1.svg" alt="BenTheHun1" width=40%></p>
+	  <ul class="contact">
+		  <li class="contact"><a href="mailto:benhunsicker1@gmail.com" class="contact"><img src="/img/mail.ico" alt="Email" Title="Email"></a></li>
+		  <li class="contact"><a href="https://twitter.com/benthehun1" class="contact"><img src="/img/tweet.ico" alt="Twitter" Title="Twitter"></a></li>
+		  <li class="contact"><a href="https://www.youtube.com/@BenTheHun1" class="contact"><img src="/img/yt.png" alt="YouTube" Title="YouTube"></a></li>
+		  <li class="contact"><a href="https://github.com/BenTheHun1" class="contact"><img src="/img/git.ico" alt="GitHub" Title="GitHub"></a></li>
+		  <li class="contact"><a href="https://www.twitch.tv/benthehun1" class="contact"><img src="/img/twitch.png" alt="Twitch" Title="Twitch"></a></li>
+	  </ul>
+	  </center>
+	  <ul class="bar">
+		  <li class="bar"><a href="/index.html">Blog</a></li>
+		  <li class="bar"><a href="/portfolio/index.html">Portfolio</a></li>
+		  <li class="bar"><a href="/mw/index.html">MiddleWorld</a></li>
+		  <li style="float:right" class="bar"><a href="/about.html" class="active">About Me</a></li>
+	  </ul>
+	  `;
+	}
+}
+
 class HeaderGeneric extends HTMLElement {
 	constructor() {
 	  super();
@@ -133,3 +160,4 @@ customElements.define('head-verse', HeaderVerse);
 customElements.define('head-advent', HeaderAdv);
 customElements.define('head-if', HeaderIF);
 customElements.define('head-generic', HeaderGeneric);
+customElements.define('head-about', HeaderAbout);
